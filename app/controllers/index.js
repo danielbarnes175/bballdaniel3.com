2bdc010e52356e0bf505a5f07987bc70be704761
Daniel Barnes <danielbarnes175@gmail.com>
2025-07-09 02:01:10 -0500

    Add recaptcha to guestbook

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -9,7 +9,10 @@ module.exports = {
     post: require("./blog").getPost,
     tag: require("./tags").getTag,
     privacy: (req, res) => res.render("privacy"),
-    guestbook: (req, res) => res.render("guestbook", { entries: guestbookController.getGuestbookEntries() }),
+    guestbook: (req, res) => {
+        let scripts = [{ script: 'https://www.google.com/recaptcha/api.js?render=6Le7JK0bAAAAADV3P6ZDDOXJHJImPrefIVghEs_7' }];
+        res.render("guestbook", { entries: guestbookController.getGuestbookEntries(), scripts: scripts });
+    },
     submitGuestbookEntry: guestbookController.submitGuestbookEntry,
     now: (req, res) => res.render("now"),
     games: (req, res) => res.render("games"),
