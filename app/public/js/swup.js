a508b3bba402c2a122b46703b0c23339ae0de338
Daniel Barnes <danielbarnes175@gmail.com>
2025-09-19 11:14:34 -0500

    Remove debug logging

--- app/public/js/swup.js
+++ app/public/js/swup.js
@@ -14,19 +14,3 @@ const options = {
 };
 
 const swup = new Swup(options);
-
-swup.hooks.on('link:click', (e) => {
-    console.log('link click');
-    swup.scrollTo(0, true);
-});
-
-swup.hooks.on('history:popstate', () => {
-    console.log('popstate');
-});
-
-swup.hooks.on('visit:start', (visit) => {
-    console.log(visit);
-});
-
-swup.hooks.on('scroll:start', () => console.log('Swup started scrolling'));
-swup.hooks.on('scroll:end', () => console.log('Swup finished scrolling'));
